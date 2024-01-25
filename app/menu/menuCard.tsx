43e633b2e07a19c8fd42d21737481be919e54a18
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2024-01-25 15:33:26 +0530

    truncate menu desc

--- app/menu/menuCard.tsx
+++ app/menu/menuCard.tsx
@@ -10,10 +10,9 @@ type MenuCardProps = {
 function MenuCard({ className, menu }: MenuCardProps) {
     return (
         <div className={className}>
-            <div className="card card-compact w-96 h-96 bg-base-100 shadow-xl">
-                <figure className="aspect-w-16 aspect-h-9"><Image
-                    className="object-cover w-full h-full"
-                    src="https://images.unsplash.com/photo-1541544537156-7627a7a4aa1c?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            <div className="card card-compact w-96 h-96 bg-base-100 shadow-xl  truncate hover:overflow-visible hover:whitespace-normal ">
+                <figure ><Image
+                    src="/menu.jpeg"
                     alt="Menu Image"
                     height={100}
                     width={400} />
